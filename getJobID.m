1700c9384b0da2943c5a5cbbc73cb41250c7b3c8
Jan Clemens <clemensjan@googlemail.com>
2015-04-13 17:18:46 -0400

    fixed extraction of JOB id from return string

--- getJobID.m
+++ getJobID.m
@@ -7,7 +7,7 @@ function jobID =getJobID(string)
 
 % we first split at 'm.o' to get a string of the form 'JOBID ' or 'JOBID.1:10:1  
 token = strsplit(string,'.m.o');
-% then split at '.' to get a string of trailing job informatio (for job arrays)
+% then split at '.' to get a string of trailing job information (for job arrays)
 token2 = strsplit(token{end},'.');
 % finally, get rid of leading 'o' and remove any trailing whitespace (for single jobs)
-jobID = strtrim(token2{2}(2:end));
+jobID = strtrim(token2{1});
