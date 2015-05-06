883f94ea4be5685e31bb77ecfa17fed2d4f95ec3
Jan Clemens <clemensjan@googlemail.com>
2015-05-05 20:39:39 -0400

    fixed job id extraction

--- getJobID.m
+++ getJobID.m
@@ -2,12 +2,13 @@ function jobID =getJobID(string)
 % get JOBID from 'submit' return string
 
 % return string of the submit command is of the form
-% 'SCRIPTNAME.m output will be in SCRIPTNAME.m.o4251723 ' for single jobs 
+% 'SCRIPTNAME.m output will be in SCRIPTNAME.m.o4251723 ' for single jobs
 % 'SCRIPTNAME.m output will be in SCRIPTNAME.m.o4251723.1-10:1 ' for array jobs
 
-% we first split at 'm.o' to get a string of the form 'JOBID ' or 'JOBID.1:10:1  
+% we first split at 'm.o' to get a string of the form 'JOBID ' or 'JOBID.1:10:1
 token = strsplit(string,'.m.o');
 % then split at '.' to get a string of trailing job information (for job arrays)
 token2 = strsplit(token{end},'.');
+jobID = strtrim(token2{1});
 % finally, get rid of leading 'o' and remove any trailing whitespace (for single jobs)
-jobID = strtrim(token2{2}(2:end));
+%    jobID = strtrim(token2{2}(2:end));
